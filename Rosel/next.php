b4625c1f30a8c22bafae53064ba3a24fc3854de7
teddrosell <roselted90@gmail.com>
2023-07-06 20:04:19 +0800

    Personal Portfolio

---
+++ Rosel/next.php
@@ -0,0 +1,25 @@
+    <!-- ======= Contact Me Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+        <div class="section-title">
+          <span>Contact Me</span>
+          <br>
+          <br>
+          <h2 style="color: #2cbdee;">Here are my contact details </h2>
+          <hr>
+          <br>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <h3 class="title"><a href=""> Email</a></h3>
+            <p class="description">tedrfael.rosel@citycollegeoftagaytay.edu.ph</p>
+          </div>
+
+          <div class="col">
+            <h3 class="title"><a href=""> Mobile Number</a></h3>
+            <p class="description">0995-543-0524</p>
+          </div>
+        </div>
+        
+    </section><!-- End Contact Me Section -->
